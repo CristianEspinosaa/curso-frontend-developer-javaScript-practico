05d582e5df258ef322613de03645e0922367df57
Cristian <cristianespinosabarajas@gmail.com>
2022-09-08 14:58:27 -0500

    fusion del carrito de compras con el produc detail

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const shoppIcon = document.querySelector('.icon-shopping')
+const productDetail = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuIcon.addEventListener('click', toggleMobileMenu)
+shoppIcon.addEventListener('click', toggleProductDetail)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
@@ -12,4 +15,9 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive')
+
+}
+
+function toggleProductDetail() {
+    productDetail.classList.toggle('inactive')
 }
